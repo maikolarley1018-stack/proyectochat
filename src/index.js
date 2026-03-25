515b4120d70a64f0e5a6f9ec57c9cf4b14dca4d2
Maikol Vega <maikolarley1018@gmail.com>
2026-03-25 00:05:11 -0500

    merge

--- src/index.js
+++ src/index.js
@@ -11,18 +11,22 @@ const io = new Server(httpServer);
 // ===== CONFIGURACIÓN =====
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ===== SOCKETS =====
-require('./sockets')(io);
-
 // ===== CONEXIÓN A MONGODB =====
-const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chat-database'
+const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chat-database';
+
 mongoose.connect(dbURI)
-.then(() => console.log('🟢 Base de datos conectada'))
+.then(() => {
+    console.log('🟢 Base de datos conectada');
+
+    // 🔥 IMPORTANTE: sockets después de conectar DB
+    require('./sockets')(io);
+
+    // ===== SERVIDOR =====
+    const PORT = process.env.PORT || 3000;
+
+    httpServer.listen(PORT, '0.0.0.0', () => {
+        console.log("🚀 Servidor en el puerto", PORT);
+    });
+
+})
 .catch(err => console.log('🔴 Error en DB:', err));
-
-// ===== SERVIDOR =====
-const PORT = process.env.PORT || 3000;
-
-httpServer.listen(app.get('port'), '0.0.0.0', () => {
-    console.log("Servidor en el puerto", app.get('port'));
-});
